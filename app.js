afce0933ea133ef23cd88111f2d5e89c9d291131
gene <softhaxorz@gmail.com>
2021-01-22 07:52:31 -0800

    add public stylesheets, display layout

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const port = 50000;
+const port = 3000;
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
+
+//const views = require("./views");
+const layout = require("./views/layout");
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res, next) => {
   try {
-    res.send("hello world");
+    res.send(layout(""));
   } catch (error) {
     next(error);
   }
